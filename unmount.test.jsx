629c6d2b39c024820bddeecdfa84021bd3ec380a
karak <karak97@gmail.com>
2018-02-27 11:01:55 +0900

    Created minimum test cases.

---
+++ unmount.test.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import withSideEffect from 'react-side-effect';
+import './setup';
+
+
+class BuggyUnmount extends React.PureComponent {
+  componentWillUnmount() {
+    throw new Error('Please catch!');
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+class Div extends React.PureComponent {
+  render() {
+    return <div/>;
+  }
+}
+
+const BuggySideEffect = withSideEffect(
+  (propsList) => { return undefined; },
+  ({ violation }) => {}
+  // "TypeError: Cannot destructure property `violation` of 'undefined' or 'null'."
+)(Div);
+
+describe('Impossible to catch errors on componentWillUnmount', () => {
+  it('Error on componentWillUnmount', () => {
+    const wrapper = mount(<BuggyUnmount/>);
+
+    expect(() => wrapper.unmount()).toThrowError('Please catch!');
+  });
+
+  it('Error through side effect', () => {
+    const wrapper = mount(<BuggySideEffect/>);
+
+    expect(() => wrapper.unmount()).toThrow();
+  });
+});
